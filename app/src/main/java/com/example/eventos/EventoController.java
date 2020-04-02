c2a2dcf4238ac6416856621625310f3d61f64493
Evandro Gonçalves <evandrolvg@gmail.com>
2020-04-02 18:27:15 -0300

    Layout

--- app/src/main/java/com/example/eventos/EventoController.java
+++ app/src/main/java/com/example/eventos/EventoController.java
@@ -8,25 +8,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.github.rtoshiro.util.format.SimpleMaskFormatter;
-import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -76,19 +69,6 @@ public class EventoController extends AppCompatActivity {
 
         eventos = new ArrayList<>();
 
-        //adding an onclicklistener to button
-        /*btnSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    addEvento();
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        });*/
-
         ltEventos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -114,7 +94,6 @@ public class EventoController extends AppCompatActivity {
         dialogBuilder.setView(dialogView);
 
         final ImageButton btnSalvar = (ImageButton) dialogView.findViewById(R.id.btnSalvar);
-        final ImageButton btnDelete = (ImageButton) dialogView.findViewById(R.id.btnDelete);
 
         final EditText etNome = (EditText) dialogView.findViewById(R.id.etNome);
         final EditText etDescricao = (EditText) dialogView.findViewById(R.id.etDescricao);
@@ -148,45 +127,6 @@ public class EventoController extends AppCompatActivity {
                 }
             }
         });
-
-
-        btnDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                /*
-                 * we will code this method to delete the artist
-                 * */
-
-            }
-        });
-    }
-
-    private void addEvento() throws ParseException {
-        String nome = etNome.getText().toString().trim();
-        String descricao = etDescricao.getText().toString().trim();
-        String data = etData.getText().toString().trim();
-        String valor = etValor.getText().toString().trim();
-        String qtdeVagas = etQtdeVagas.getText().toString().trim();
-        String localRealizacao = etLocalRealizacao.getText().toString().trim();
-
-        if (!TextUtils.isEmpty(nome) && !TextUtils.isEmpty(descricao) && !TextUtils.isEmpty(data) && !TextUtils.isEmpty(valor) && !TextUtils.isEmpty(qtdeVagas) && !TextUtils.isEmpty(localRealizacao)) {
-            String id = databaseEventos.push().getKey();
-
-            //Date dataParse = new SimpleDateFormat("dd/MM/yyyy").parse(data);
-            double valorParse = Double.parseDouble(valor);
-            int qtdeVagasParse = Integer.parseInt(qtdeVagas);
-            Evento evento = new Evento(id, nome, descricao, data, valorParse, qtdeVagasParse, localRealizacao);
-
-            databaseEventos.child(id).setValue(evento);
-            //etNome.setText("");
-
-            Toast.makeText(this, "Evento cadastrado com sucesso.", Toast.LENGTH_LONG).show();
-        } else {
-            Toast.makeText(this, "Por favor, preencha todos os campos", Toast.LENGTH_LONG).show();
-        }
-
-        atualizaLista();
     }
 
     private void showUpdateDeleteDialog(final String id, String nome, String descricao, String data, Double valor, int qtdeVagas, String localRealizacao) {
@@ -231,37 +171,24 @@ public class EventoController extends AppCompatActivity {
             }
         });
 
-
         btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                /*
-                 * we will code this method to delete the artist
-                 * */
-
+                removeEvento(id);
+                b.dismiss();
             }
         });
     }
 
     private boolean addUpdateEvento(String id, String nome, String descricao, String data, Double valor, int qtdeVagas, String localRealizacao) {
-        //getting the specified artist reference
-        //DatabaseReference dR = FirebaseDatabase.getInstance().getReference("eventos").child(id);
-
-        Log.d("update", "id: " + id);
-
-        if (!TextUtils.isEmpty(id)){
+        Log.d("ID", "ID:" + id);
+        if (!TextUtils.isEmpty(id) && !id.equals(NULL)){
             Evento evento = new Evento(id, nome, descricao, data, valor, qtdeVagas, localRealizacao);
-            //dR.setValue(evento);
-            Log.d("teste", nome);
+
             databaseEventos.child(id).setValue(evento);
             Toast.makeText(getApplicationContext(), "Evento atualizado", Toast.LENGTH_LONG).show();
         }else{
             String idAdd = databaseEventos.push().getKey();
-
-            //Date dataParse = new SimpleDateFormat("dd/MM/yyyy").parse(data);
-            //double valorParse = Double.parseDouble(valor);
-            //int qtdeVagasParse = Integer.parseInt(qtdeVagas);
             Evento evento = new Evento(idAdd, nome, descricao, data, valor, qtdeVagas, localRealizacao);
 
             databaseEventos.child(idAdd).setValue(evento);
@@ -270,6 +197,17 @@ public class EventoController extends AppCompatActivity {
             Toast.makeText(this, "Evento cadastrado com sucesso.", Toast.LENGTH_LONG).show();
         }
 
+        return true;
+    }
+
+    private boolean removeEvento(String id) {
+        if (!TextUtils.isEmpty(id)){
+            databaseEventos = FirebaseDatabase.getInstance().getReference("eventos").child(id);
+            databaseEventos.removeValue();
+            Toast.makeText(getApplicationContext(), "Evento removido", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(getApplicationContext(), "Ocorreu algum erro", Toast.LENGTH_LONG).show();
+        }
 
         return true;
     }
@@ -283,21 +221,13 @@ public class EventoController extends AppCompatActivity {
         databaseEventos.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-
-                //clearing the previous artist list
                 eventos.clear();
-
-                //iterating through all the nodes
                 for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
-                    //getting artist
                     Evento evento = postSnapshot.getValue(Evento.class);
-                    //adding artist to the list
                     eventos.add(evento);
                 }
 
-                //creating adapter
                 EventoList eventoAdapter = new EventoList(EventoController.this, eventos);
-                //attaching adapter to the listview
                 ltEventos.setAdapter(eventoAdapter);
             }
 
--- app/src/main/java/com/example/eventos/MainActivity.java
+++ app/src/main/java/com/example/eventos/MainActivity.java
@@ -1,75 +1,44 @@
 package com.example.eventos;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import com.github.rtoshiro.util.format.SimpleMaskFormatter;
-import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 import com.google.firebase.database.ValueEventListener;
-
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ListView ltEventos;
-
     List<Evento> eventos;
-
-    /*public static final String ARTIST_NAME = "com.example.eventos.artistname";
-    public static final String ARTIST_ID = "com.example.eventos.artistid";*/
-
     DatabaseReference databaseEventos;
-    /*private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
-    private DatabaseReference eventoReferencia = FirebaseDatabase.getInstance().getReference("eventos");*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         databaseEventos = FirebaseDatabase.getInstance().getReference("eventos");
-
         ltEventos = (ListView) findViewById(R.id.ltEventos);
-
         eventos = new ArrayList<>();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
         databaseEventos.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-
-                //clearing the previous artist list
                 eventos.clear();
-
-                //iterating through all the nodes
                 for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
-                    //getting artist
                     Evento evento = postSnapshot.getValue(Evento.class);
-                    //adding artist to the list
                     eventos.add(evento);
                 }
-
-                //creating adapter
                 EventoList eventoAdapter = new EventoList(MainActivity.this, eventos);
-                //attaching adapter to the listview
                 ltEventos.setAdapter(eventoAdapter);
             }
 
